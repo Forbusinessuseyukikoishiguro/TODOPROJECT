7d05af43349ed5a743950dd7a699266bea7f4675
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-07 16:22:36 +0900

    TODOAPPの管理画面機能追加

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TOdoModel
 
 # Register your models here.
+admin.site.register(TOdoModel)
+# ここでTodoModelをadminページに登録している
+# これでadminページでTodoModelを見れるようになるよ！
