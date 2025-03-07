c9a14b421210aa24133f15c77ae0bb88cefad4b5
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-07 15:40:46 +0900

    マイグレーション1540

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class TOdoModer(models.Model):
+class TOdoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
