84b7190bf0308a2ef3141ae642011535ffc4ff56
Forbusinessuseyukikoishiguro <yukinote1026@gmail.com>
2025-03-11 16:53:09 +0900

    締め切りと優先順位で色をかえる設定完了

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
+CHOICE =(('danger','high'),('warning','normal'),('primary','low'))
 
-# Create your models here.
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
-
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE,
+        default='primary'  # デフォルト値を設定
+    )
+    duedate = models.DateField(default=timezone.now)  # デフォルト値を設定
+    
     def __str__(self):
         return self.title
-
-    # super().__str__()
